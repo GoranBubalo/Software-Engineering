01dda0fb7e08876dff9ca21df13fe324ec39623b
Goran Bubalo <goranbubalo2422003@gmail.com>
2025-09-01 23:58:36 +0200

    refactoring modul for getting important info

--- WebScraping/scraper_functions.py
+++ WebScraping/scraper_functions.py
@@ -3,10 +3,4 @@ from bs4 import BeautifulSoup
 
 # Getting details 
 def scrape_dom_details(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an error for bad responses
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        nursing_home_details = {}
-        nursing_home_details['URL'] = url
+    pass
