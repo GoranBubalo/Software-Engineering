f3f79238b65f78bd5e62f02d246c0a9ad4460d29
Goran Bubalo <goranbubalo2422003@gmail.com>
2025-09-19 12:50:20 +0200

    Adding scanf so that the user can input an array of characters => name

--- foundations/setup_hello_word.c
+++ foundations/setup_hello_word.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+
+    char name[50];
+
     printf("Hello, Goran!\n");
-    return 0;
+    
+    printf("Enter your last name: ");
+    scanf("%49s", name);
+    printf("Gorans last name is %s, welcomme to c programming\n", name);
 }
